9ec3e1de4ccfb4d4778c93fff1553d088d3741f2
judith-vm <157530840+judith-vm@users.noreply.github.com>
2024-02-13 19:12:46 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var socket = require('socket.io');
+
+const port = process.env.PORT || 4000;
+// App setup
+var app = express();
+var server = app.listen(port, function(){
+    console.log('server on port',port);
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
